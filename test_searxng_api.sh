aabbcb04763ef368df8e22d2b38fd815ba8ea3c9
localaivn <thanhtan.tran@gmail.com>
2026-04-19 10:46:01 +0700

    update test

--- test_searxng_api.sh
+++ test_searxng_api.sh
@@ -2,7 +2,7 @@
 # Script để test SearxNG API với API key
 
 API_KEY="sk-searxng-4f0158d9fc0a9750d55e338fef1092f0"
-SEARXNG_URL="https://searxng.ttcorp.net"
+SEARXNG_URL="http://127.0.0.1:8088"
 
 echo "=== Testing SearxNG API with API Key ==="
 echo ""
